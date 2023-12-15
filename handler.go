ba1163aa4c02ef5ed8d5fd1d5bb601d7365c38f6
cinnak <121988836+cinnak@users.noreply.github.com>
2023-12-15 15:57:44 +0000

    http.handler

--- handler.go
+++ handler.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-
+	"fmt"
 	"gopkg.in/yaml.v2"
 )
 
@@ -16,7 +16,14 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request){
+		path := r.URL.Path
+		if dest,ok := pathsToUrls[path];ok {
+			http.Redirect(w,r,dest,http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w,r)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -37,35 +44,10 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // a mapping of paths to urls.
 func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	// 1. parse the yaml somehow
-	pathUrls,err := parseYaml(yamlBytes)
-	if err != nil {
-		return nil,err
-	}
+	
 	// 2. convert yaml array into map
-	pathToUrls := buildMap(pathUrls)
+	
 	// 3. return a map handler using the map
-	return MapHandler(pathsToUrls, fallback), nil
+	
 }
 
-type pathUrl struct {
-	Path string `yaml:"path"`
-	URL  string `yaml:"url"`
-}
-
-func parseYaml(data bytes[]) ([]pathUrl, error) {
-	var pathUrls []pathUrl
-	err := yaml.Unmarshal(data, &pathUrls)
-	if err != nil {
-		return nil, err
-	}
-	return pathUrls,nil
-}
-
-func buildMap(pathUrls []pathUrl) map[string]string {
-	pathsToUrls := make(map[string]string)
-	for _, pu := range pathUrls{
-		pathsToUrls[pu.Path] =pu.URL
-	}
-	return pathsToUrls
-
-}
